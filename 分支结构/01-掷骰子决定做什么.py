db82cffb8bd72bd983ff514fb351b11c928aa255
diligent-1618 <47840359+diligent-1618@users.noreply.github.com>
2019-06-16 20:39:34 +0800

    第二天答案
    第二天答案

---
+++ 分支结构/01-掷骰子决定做什么.py
@@ -0,0 +1,17 @@
+from random import randint
+
+face = randint(1, 6)
+if face == 1:
+    result = '唱首歌'
+elif face == 2:
+    result = '跳个舞'
+elif face == 3:
+    result = '学狗叫'
+elif face == 4:
+    result = '做俯卧撑'
+elif face == 5:
+    result = '念绕口令'
+else:
+    result = '讲冷笑话'
+print(result)
+# 上面的代码中使用了random模块的randint函数生成指定范围的随机数来模拟掷骰子。
